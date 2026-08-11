6f6ed348430af934d2cc6a8d45abaa111f458a17
Arthur1323432435 <abreu.arthur@discente.ufma.br>
2026-08-10 22:53:43 -0300

    commit inicial

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Libera conexões de Server Actions e origens locais
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '192.168.1.13:3000'],
+    },
+  },
 };
 
 export default nextConfig;
